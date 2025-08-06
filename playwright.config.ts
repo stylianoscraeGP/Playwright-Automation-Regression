d6edbc7b4fc1eeba37d0ea0c399aa35821ee1dd1
stylianoscraeGP <stylianos@gaem.io>
2025-08-06 17:31:41 +0300

    Merging Browser Stack in Git Hub

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,35 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const browserstackUsername = process.env.BROWSERSTACK_USERNAME;
+const browserstackAccessKey = process.env.BROWSERSTACK_ACCESS_KEY;
+
 export default defineConfig({
   testDir: './RegressionAutomation',  // your test folder
   projects: [
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },  // run tests only on Firefox
+      use: { ...devices['Desktop Firefox'] },  // local Firefox
+    },
+    {
+      name: 'bs-chrome-win',
+      use: {
+        browserName: 'chromium',
+        // Connect to BrowserStack remote browser via WebSocket endpoint
+        connectOptions: {
+          wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(
+            JSON.stringify({
+              browser: 'chrome',
+              browser_version: 'latest',
+              os: 'Windows',
+              os_version: '10',
+              name: 'Playwright test on BrowserStack',
+              build: 'build-1',
+              'browserstack.username': browserstackUsername,
+              'browserstack.accessKey': browserstackAccessKey,
+            })
+          )}`,
+        },
+      },
     },
   ],
   reporter: [['list'], ['html']],
@@ -14,7 +38,7 @@ export default defineConfig({
     screenshot: 'on',
     video: 'on',
     launchOptions:{
-      devtools: true,  // enable devtools for debugging
+      devtools: true,
     }
   },
 });
